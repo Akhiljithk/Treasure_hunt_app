821b1be169056f5e086b687993a6196232f8b82b
akhiljith <akhiljithk112@gmail.com>
2021-06-24 13:05:50 +0530

    music indicator and music added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,3 +4,23 @@ function changeBtnText(){
       document.getElementById("btn-text").innerHTML  = '<i class="fa fa-lock"></i> Unlock';
     }, 3000)
 }
+
+function play() {
+    var audio = document.getElementById('audio');
+    var playBtn = document.getElementById('play');
+    var animation = document.getElementById('music-animation');
+    if (audio.paused) {
+        animation.style.display = "block";
+        playBtn.style.display = "none";
+        audio.play();
+        $('#play').removeClass('glyphicon-play-circle')
+        $('#play').addClass('glyphicon-pause')
+    }else{
+        animation.style.display = "none";
+        playBtn.style.display = "block";
+        audio.pause();
+        audio.currentTime = 0
+        $('#play').addClass('glyphicon-play-circle')
+        $('#play').removeClass('glyphicon-pause')
+    }
+}
